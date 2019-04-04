d240d9bb9feb091bd0ee4de24fa7170d8e2f99f3
Justin Hollmann <justin.hollmann.jh@gmail.com>
2019-04-04 09:32:45 +0200

    User bearbeiten

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/Malteser/einsaetze/jpa/Einsatz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbwka.wwi.vertsys.javaee.Malteser.einsaetze.jpa;
+
+
+public class Einsatz {
+    
+//    private Ort ort;
+//    private Date startDatum;
+//    private Date endDatum;
+//    private String einsatzart;
+//    private Rettungsmittel rettungsmittel; //Liste
+//    private String Ansprechpartner;
+//    private String gliederung;
+    
+}
--- src/main/java/dhbwka/wwi/vertsys/javaee/Malteser/tasks/web/UserEditServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/Malteser/tasks/web/UserEditServlet.java
@@ -9,12 +9,11 @@
  */
 package dhbwka.wwi.vertsys.javaee.Malteser.tasks.web;
 
-import dhbwka.wwi.vertsys.javaee.Malteser.common.web.FormValues;
-import dhbwka.wwi.vertsys.javaee.Malteser.tasks.ejb.CategoryBean;
-import dhbwka.wwi.vertsys.javaee.Malteser.tasks.ejb.TaskBean;
+import dhbwka.wwi.vertsys.javaee.Malteser.common.ejb.UserBean;
 import dhbwka.wwi.vertsys.javaee.Malteser.common.ejb.ValidationBean;
-import dhbwka.wwi.vertsys.javaee.Malteser.tasks.jpa.Category;
-import dhbwka.wwi.vertsys.javaee.Malteser.tasks.jpa.Task;
+import dhbwka.wwi.vertsys.javaee.Malteser.common.jpa.User;
+import dhbwka.wwi.vertsys.javaee.Malteser.common.web.FormValues;
+import dhbwka.wwi.vertsys.javaee.Malteser.common.web.WebUtils;
 import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
@@ -34,15 +33,19 @@ import javax.servlet.http.HttpSession;
 @WebServlet(urlPatterns = {"/app/tasks/useredit/"})
 public class UserEditServlet extends HttpServlet {
 
+@EJB
+private UserBean userBean;
 
+@EJB
+private ValidationBean validationBean;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
  
         HttpSession session = request.getSession();
-        /*session.setAttribute("username", user.getUsername() )*/
-        // Anfrage an dazugerhörige JSP weiterleiten
+        User user = this.userBean.getCurrentUser();
+        session.setAttribute("user", user);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/tasks/useredit.jsp");
         dispatcher.forward(request, response);
 
@@ -51,7 +54,42 @@ public class UserEditServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        User aktuellerUser = this.userBean.getCurrentUser();
+        
+        String vorname = request.getParameter("signup_vorname");
+        String nachname = request.getParameter("signup_nachname");
+        String password1 = request.getParameter("signup_password1");
+        String password2 = request.getParameter("signup_password2");
+        
+        User user = new User(vorname, nachname, aktuellerUser.getUsername(), password1);
+        List<String> errors = this.validationBean.validate(user);
+        this.validationBean.validate(user.getPassword(), errors);
+        
+        if (password1 != null && password2 != null && !password1.equals(password2)) {
+            errors.add("Die beiden Passwörter stimmen nicht überein.");
+        }
+        
+        if (errors.isEmpty()) {
+            try {
+                aktuellerUser.setVorname(vorname);
+                aktuellerUser.setNachname(nachname);
+                aktuellerUser.setPasswordHash(password2);
+                aktuellerUser.addToGroup("app-user");
+                this.userBean.update(aktuellerUser);
+               
+            } catch (Exception ex) {
+                errors.add(ex.getMessage());
+            }
+        }
+        
+        if (errors.isEmpty()) {
+            response.sendRedirect(WebUtils.appUrl(request, "/app/dashboard/"));
+        } else {            
+            response.sendRedirect(WebUtils.appUrl(request, "/app/dashboard/"));
+        }
+        
+        this.userBean.update(user);
     }
 
 
