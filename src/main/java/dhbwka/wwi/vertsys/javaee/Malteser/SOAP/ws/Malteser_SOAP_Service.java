967b5cb3a3055ec059cee9dc48bd60b47ab086e1
Jonas Beuth <jonasbeuth98@freenet.de>
2019-04-05 09:20:33 +0200

    Namensaenderung

--- src/main/java/dhbwka/wwi/vertsys/javaee/Malteser/SOAP/ws/Malteser_SOAP_Service.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/Malteser/SOAP/ws/Malteser_SOAP_Service.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Malteser_SOAP_Service;
+package dhbwka.wwi.vertsys.javaee.Malteser.SOAP.ws;
 
 /**
  *
